312e948aaf442dc251662a5af03d7a9813421516
jaswanth <lci2022010@iiitl.ac.in>
2024-02-05 18:26:58 +0530

    my commit

---
+++ lib/utils.ts
@@ -0,0 +1,122 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const categories = [
+  {
+    id: "6540bbda311501fafc4f9c79",
+    name: "Technology",
+  },
+  {
+    id: "6540bc13311501fafc4f9c7f",
+    name: "Healthcare",
+  },
+  {
+    id: "6540bbf4311501fafc4f9c7d",
+    name: "Food",
+  },
+  {
+    id: "6540bbe6311501fafc4f9c7b",
+    name: "Education",
+  },
+  {
+    id: "6540bbca311501fafc4f9c77",
+    name: "Politics",
+  },
+];
+export const blogs = [
+  {
+    id: "64a9913d800a1688b9b761ab",
+    title: "NextJS is Aweasome",
+    description:
+      " <p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>,<p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p><p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>,<p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p><p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>",
+    imageUrl:
+      "http://res.cloudinary.com/newtestenv/image/upload/v1688834369/nextjs-full-stack-blog/tokgnonf1pbbtef4undi.jpg",
+    userId: "64a449a19fc3d7819657cb6a",
+    createdAt: "2023-07-08T16:39:25.055Z",
+    updatedAt: "2023-07-08T16:43:31.572Z",
+    categoryId: "64a98a4c800a1688b9b761a6",
+    location: "India",
+  },
+  {
+    id: "64a9913d800a1688b9b761a12",
+    title: "NextJS is Aweasome",
+    description:
+      " <p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>,<p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p><p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>",
+    imageUrl:
+      "http://res.cloudinary.com/newtestenv/image/upload/v1688834369/nextjs-full-stack-blog/tokgnonf1pbbtef4undi.jpg",
+    userId: "64a449a19fc3d7819657cb6a",
+    createdAt: "2023-07-08T16:39:25.055Z",
+    updatedAt: "2023-07-08T16:43:31.572Z",
+    categoryId: "64a98a4c800a1688b9b761a6",
+    location: "India",
+  },
+  {
+    id: "64a9913d800a1688b9b761ab123",
+    title: "NextJS is Aweasome",
+    description:
+      " <p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>,<p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p><p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>",
+    imageUrl:
+      "http://res.cloudinary.com/newtestenv/image/upload/v1688834369/nextjs-full-stack-blog/tokgnonf1pbbtef4undi.jpg",
+    userId: "64a449a19fc3d7819657cb6a",
+    createdAt: "2023-07-08T16:39:25.055Z",
+    updatedAt: "2023-07-08T16:43:31.572Z",
+    categoryId: "64a98a4c800a1688b9b761a6",
+    location: "India",
+  },
+  {
+    id: "64a9913d800a1688b9b761ab23323",
+    title: "NextJS is Aweasome",
+    description:
+      " <p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>,<p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p><p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>",
+    imageUrl:
+      "http://res.cloudinary.com/newtestenv/image/upload/v1688834369/nextjs-full-stack-blog/tokgnonf1pbbtef4undi.jpg",
+    userId: "64a449a19fc3d7819657cb6a",
+    createdAt: "2023-07-08T16:39:25.055Z",
+    updatedAt: "2023-07-08T16:43:31.572Z",
+    categoryId: "64a98a4c800a1688b9b761a6",
+    location: "India",
+  },
+  {
+    id: "64a9913d800a1688b9b761ab22323",
+    title: "NextJS is Aweasome",
+    description:
+      " <p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>,<p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p><p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>",
+    imageUrl:
+      "http://res.cloudinary.com/newtestenv/image/upload/v1688834369/nextjs-full-stack-blog/tokgnonf1pbbtef4undi.jpg",
+    userId: "64a449a19fc3d7819657cb6a",
+    createdAt: "2023-07-08T16:39:25.055Z",
+    updatedAt: "2023-07-08T16:43:31.572Z",
+    categoryId: "64a98a4c800a1688b9b761a6",
+    location: "India",
+  },
+  {
+    id: "64a9913d800a1688b9b761ab232323",
+    title: "NextJS is Aweasome",
+    description:
+      " <p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>,<p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p><p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>",
+    imageUrl:
+      "http://res.cloudinary.com/newtestenv/image/upload/v1688834369/nextjs-full-stack-blog/tokgnonf1pbbtef4undi.jpg",
+    userId: "64a449a19fc3d7819657cb6a",
+    createdAt: "2023-07-08T16:39:25.055Z",
+    updatedAt: "2023-07-08T16:43:31.572Z",
+    categoryId: "64a98a4c800a1688b9b761a6",
+    location: "India",
+  },
+  {
+    id: "64a9913d800a1688b9b761ab121212",
+    title: "NextJS is Aweasome",
+    description:
+      " <p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>,<p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p><p>Hello, NextJS is really aweasome. </p><p>You shoud learn it. </p>",
+    imageUrl:
+      "http://res.cloudinary.com/newtestenv/image/upload/v1688834369/nextjs-full-stack-blog/tokgnonf1pbbtef4undi.jpg",
+    userId: "64a449a19fc3d7819657cb6a",
+    createdAt: "2023-07-08T16:39:25.055Z",
+    updatedAt: "2023-07-08T16:43:31.572Z",
+    categoryId: "64a98a4c800a1688b9b761a6",
+    location: "India",
+  },
+];
---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: [
+        "images.unsplash.com",
+        "res.cloudinary.com",
+        "avatars.githubusercontent.com",
+        "lh3.googleusercontent.com",
+        "img.freepik.com" 
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
